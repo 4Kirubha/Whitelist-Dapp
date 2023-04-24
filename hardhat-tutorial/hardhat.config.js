88bfdffe552573e4facd17532c39b3de43be4dd4
4Kirubha <jaimelannister4221@gmail.com>
2023-04-24 17:10:24 +0530

    First commit

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks:{
     sepolia:{
       url: "https://sepolia.infura.io/v3/e9cf275f1ddc4b81aa62c5aa0b11ac0f",
-      accounts: ["4c07e1ac8e50f79c66f986b7eee80b91f0c8460bd1481d59781893fdae4cb3f6"],
+      accounts: [""],
     },
   },
 };
